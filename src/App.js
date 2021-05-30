461558f91ec8c11cd015eba173fc9740543488c8
Vivek V <iamvivekv1996@yahoo.in>
2021-05-31 00:34:57 +0530

    :sparkles: added bootstrap navbar component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import Register from './pages/Register';
+import Register from "./pages/Register";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav />
         <Route path="/" exact component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
