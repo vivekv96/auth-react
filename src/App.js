492099eff2889dc40430ca7fbd909a47525c5999
Vivek V <iamvivekv1996@yahoo.in>
2021-05-30 23:55:53 +0530

    :art: added bootstrap CDN, and copied signin template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form-signin">
+        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <input type="email" className="form-control" placeholder="Email" required />
+        <input type="password" className="form-control" placeholder="Password" required />
+        <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
     </div>
   );
 }
